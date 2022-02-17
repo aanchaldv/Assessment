ebfd4322e779483bf95c068993c1b3ea35cc10cd
aanchaldv <54144454+aanchaldv@users.noreply.github.com>
2022-02-17 16:37:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function onButtonClick(param){
+    document.getElementById(param).classList.remove("hide");
+    document.getElementById("buttonReadMore").classList.remove("hide");  
+  }
+  
+  function readMore(){
+    document.getElementById("box02").style.height= "8vw";
+    document.getElementById("box02").style.color= "white";
+  }
+ 
+
+ 
